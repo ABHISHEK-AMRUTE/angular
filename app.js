3b2eba7a16aba9babd9c25da71a5e5c525d808c2
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-25 09:05:37 +0530

    updated

--- app.js
+++ app.js
@@ -10,7 +10,19 @@ var real_services=[
   { text: "HbA1c",id: 7 ,state:"off" },
   { text: "Blood-Glucose-Fasting",id: 8 ,state:"off" }, {text: "Vitamin-B12-Cyanocobalamin",id: 9 ,state:"off" },{ text: "Vitamin-D-Total-25-Hydroxy",id: 10,state:"off"  },{ text: "HPlus-Healthy-2020-Package",id: 11,state:"off"},{ text: "Healthians-Full-Body-Checkup-with-Thyroid-and-CBC ", id: 12,state:"off"},{ text: "Basic-Screening-Package", id: 13,state:"off"},{ text: "Healthians-Basic-Health-Checkup", id: 14,state:"off"},{ text: "Healthy-Indian-Days-Special-Package", id: 15,state:"off"},{ text: "OnePlusOne-Extended-Package-1599", id: 16,state:"off"},{ text: "OnePlusOne-Be-Healthy-Package", id: 17,state:"off"},{ text: "STI-Detection-Package", id: 18,state:"off"},{ text: "Healthians-Low-Energy-checkup", id: 19,state:"off"}]
 
+app.config(['$locationProvider', function ($locationProvider) {
 
+    if (window.history && window.history.pushState) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: true,
+            rewriteLinks: false
+        });
+    }
+    else {
+        $locationProvider.html5Mode(false);
+    }
+}]);
 app.controller('MainCtrl', function($scope, $http) {
   $scope.tags = [
     
@@ -37,38 +49,91 @@ app.controller('MainCtrl', function($scope, $http) {
         id:$tag.id,state:"on"
       }
       arr.push(ob);
-      v()
+      
 
       real_services.forEach(element => {
         if(element.text==$tag.text)
         {
           element.state="on"
         }
-        console.log(element)
+        
       });
-    
+      console.log(arr)
       
      
   }
-   $scope.onTagRemoved = function($tag) {
+
+  $scope.onTagRemoved = function($tag) {
     
-      v()
+     
     real_services.forEach(element => {
       if(element.text==$tag.text)
       {
         element.state="off"
       }
-      console.log(element)
+     
     });
   
   }
+ 
+
+ $scope.v = function (text)
+  { 
+    console.log(text)
+  
+    real_services.forEach(element => {
+     if(element.text==text||element.id==text)
+     {
+           if(element.state=='off')
+           {
+               element.state='on'
+               $scope.tags.push({
+                text:element.text,
+                id:element.id
+              })
+              arr.push({
+                text:element.text,
+                id:element.id
+              })
+               
+           }
+           console.log(arr)
+     }
+    });
+    
+  };
+  
 });
 
-function load_list()
-{
-  var 
-}
-function v(text)
-{
-  console.log(text)
-}
+
+// load()
+// function load()
+// { 
+//   var tabel = document.getElementById('table')
+//   tabel.innerHTML= ""
+//   var tr = document.createElement('tr')
+//   var th =  document.createElement('th')
+//   th.innerHTML="Popular Services"
+//   tr.append(th)
+//   tabel.append(tr)
+  
+
+//   real_services.forEach(element => {
+   
+//     var tr = document.createElement('tr')
+//     var td =  document.createElement('td')
+//     var td1 =  document.createElement('td')
+//     td.innerHTML = element.text
+//     tr.setAttribute('id','table_row')
+//     td.setAttribute('ng-click','v(this.innerHTML)')
+//     td1.setAttribute('ng-click','v(this.innerHTML)')
+//     if(element.state=='on') td1.innerHTML = "-"
+//     else  td1.innerHTML = "+"
+//     tr.append(td)
+//     tr.append(td1)
+//     tabel.append(tr)
+//   });
+
+
+
+// }
