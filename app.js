f8e6e9ebf44235774ea8f0a5e05ca35f5192c96e
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-25 11:48:22 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var app = angular.module('plunker', ['ngTagsInput']);
 var arr =[];
+var final_amount=0;
 var real_services=[
   { text: "Lipid-Profile",id: 1 ,state:"off"},
   { text: "Liver-Function Test",id: 2 ,state:"off" },
@@ -59,6 +60,7 @@ app.controller('MainCtrl', function($scope, $http) {
         
       });
       console.log(arr)
+      generate_results()
       
      
   }
@@ -73,7 +75,7 @@ app.controller('MainCtrl', function($scope, $http) {
       }
      
     });
-  
+    generate_results()
   }
  
 
@@ -105,35 +107,121 @@ app.controller('MainCtrl', function($scope, $http) {
   
 });
 
+function generate_results()
+{
+  document.getElementById('package_info').style.display='block'
+  var st =""
+  var c=1;
+  arr.forEach(element => {
+    if(c%2==0){
+      st+=element.text+"<br>"
+    c++;
+    }
+    else{
+    c++;
+    st+=element.text+" , "
+    }
+      
+  });
+  document.getElementById('content_of_package').innerHTML=st;
+  var price = 99*arr.length
+  document.getElementById('price_package').innerHTML=price ;
+  var big_ele =document.getElementById('individual_results')
+{/* <div-container  class="w3-container" style="margin-top: 50px;">
+      <div-row class='w3-row w3-border'>
 
-// load()
-// function load()
-// { 
-//   var tabel = document.getElementById('table')
-//   tabel.innerHTML= ""
-//   var tr = document.createElement('tr')
-//   var th =  document.createElement('th')
-//   th.innerHTML="Popular Services"
-//   tr.append(th)
-//   tabel.append(tr)
+        <div-row1 class="w3-col l5 m5" style="vertical-align: middle;">
+          <div-divimg class='w3-card ' style="float:right;vertical-align: middle;">
   
+            <img src="./hormones.png" width="300px" height="300px" class='w3-img w3-padding' >
+          </div>
+        </div>
 
-//   real_services.forEach(element => {
+        <div-row2 class="w3-col l7 m7">
+          <div-mmain class='' style="float:left;padding:50px;">
+            <h1- class='w3-xxlarge' id="heading"
+              style="text-align:center;margin-top:10px; font-family: 'Times New Roman', Times, serif;"><b><i>Your Complete Package</i></b></h1>
+            <p  id='content_of_package' class='w3-large' style="font-family: 'Times New Roman', Times, serif;text-align:center">
+            </p>
+            <div-center style="text-align: center;">
+              <div-price id="pack_price"class='w3-xxlarge' style='font-style: italic;color:green;font-weight:bold'>RS 100</div>
+            
+              <button class="w3-button w3-white w3-border w3-border-blue w3-round-large">Buy Now</button>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+  
+    </div> */}
+  arr.forEach(element => {
+    var  container= document.createElement('div')
+    var row = document.createElement('div')
+    var row1 = document.createElement('div')
+    var row2 = document.createElement('div')
+    var  divimg= document.createElement('div')
+    var  img= document.createElement('img')
+    var  mmain= document.createElement('div')
+    var  h1= document.createElement('h1')
+    var  p= document.createElement('p')
+    var  center= document.createElement('div')
+    var  price= document.createElement('div')
+    var  button= document.createElement('button')
+    divimg.append(img)
+    row1.append(divimg)
+    center.append(price)
+    center.append(button)
+    mmain.append(h1)
+    mmain.append(p)
+    mmain.append(center)
+    row2.append(mmain)
+    row.append(row1)
+    row.append(row2)
+    container.append(row)
+    container.setAttribute('class','w3-container')
+    container.setAttribute('style','margin-top: 50px;')
+    row.setAttribute('class','w3-row w3-border')
+    row1.setAttribute('class','w3-col l5 m5')
+    row1.setAttribute('style','vertical-align: middle;')
+    divimg.setAttribute('class','w3-card')
+    divimg.setAttribute('style','float:right;vertical-align: middle;')
+    img.setAttribute('src','./hormones.png')
+    img.setAttribute('style','width: 300px;height: 300px;')
+    img.setAttribute('class','w3-img w3-padding')
+    row2.setAttribute('class','w3-col l7 m7')
+    mmain.setAttribute('style','float:left;padding:50px;')
+    h1.setAttribute('class','w3-xxlarge')
+    
+    h1.setAttribute('style',"text-align:center;margin-top:10px; font-family: 'Times New Roman', Times, serif;")
    
-//     var tr = document.createElement('tr')
-//     var td =  document.createElement('td')
-//     var td1 =  document.createElement('td')
-//     td.innerHTML = element.text
-//     tr.setAttribute('id','table_row')
-//     td.setAttribute('ng-click','v(this.innerHTML)')
-//     td1.setAttribute('ng-click','v(this.innerHTML)')
-//     if(element.state=='on') td1.innerHTML = "-"
-//     else  td1.innerHTML = "+"
-//     tr.append(td)
-//     tr.append(td1)
-//     tabel.append(tr)
-//   });
+    p.setAttribute('class','w3-large')
+    p.setAttribute('style',"font-family: 'Times New Roman', Times, serif;text-align:center")
+    center.setAttribute('style','text-align: center;')
+    
+    price.setAttribute('class','w3-xxlarge')
+    price.setAttribute('style','font-style: italic;color:green;font-weight:bold')
+    button.setAttribute('class','w3-button w3-white w3-border w3-border-blue w3-round-large')
+    button.setAttribute('data-target','#login')
+    button.setAttribute('data-toggle','modal')
+    h1.innerHTML = element.text
+    console.log('adding : '+element.text)
+    p.innerHTML = "Secure trusted and 100% effective<br>Hurry up!" 
+    price.innerHTML = "Rs 99"
+    big_ele.append(container)
+    button.innerHTML = "Buy Now"
+  });
 
-
-
-// }
+}
+function save(){
+  final_amount = document.getElementById('price_package').innerHTML;
+  console.log(final_amount);
+}
+function show(){
+  document.getElementById('payment').style.display='block'
+  if(final_amount!=0)
+  document.getElementById('final_invoice_price').innerHTML = final_amount
+  else document.getElementById('final_invoice_price').innerHTML = 99
+  document.getElementById('package_info').style.display='none'
+  
+  document.getElementById('individual_results').style.display='none'
+}
